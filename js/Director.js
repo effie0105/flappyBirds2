cc673fbecd82904ce9a423ade93da1405628189b
vane <1765881731@qq.com>
2019-10-22 20:24:06 +0800

    小鸟的三条命的代码

--- js/Director.js
+++ js/Director.js
@@ -1,17 +1,16 @@
 import { DataStore } from "./base/DataStore.js";
 import { UpPipe } from "./runtime/UpPipe.js";
 import { DownPipe } from "./runtime/DownPipe.js";
-import { Tool } from "../extra.js";
+import { Tool } from '../extra.js';
 
+// 导演类，控制游戏的主流程，逻辑
 
-//导演类,控制游戏的主流程,逻辑
 export class Director{
   constructor(){
-    //获取变量池
+    // 获取变量池
     this.dataStore = DataStore.getInstance();
   }
-
-  //导演只能有一个
+  // 导演只能有一个
   static getInstance(){
     if(!Director.instance){
       Director.instance = new Director();
@@ -19,136 +18,184 @@ export class Director{
     return Director.instance;
   }
 
-  //判断小鸟与某一个水管是否碰撞(小鸟的模型，某个水管的模型)
+  change(){
+    // 死了一条命，切换无敌状态
+    let wudi = this.dataStore.get('birds').wudi;
+    if(wudi){
+      setTimeout(()=>{
+        console.log(wudi);
+        this.dataStore.get('birds').wudi = false;
+      },1000);
+    }
+  }
+
+  // 判断小鸟与某一个水管是否碰撞(小鸟的模型，某个水管的模型)
   isStrike(bird,pipe){
-    let strike = true;//假设撞上了
-    //不撞上的条件
-    if(bird.right<pipe.left || bird.bottom<pipe.top ||bird.left>pipe.right || bird.top>pipe.bottom){
-      //小鸟的右边小于水管的左边 || 小鸟的下边小于水管的上边 ||小鸟的左边要大于水管的右边 || 小鸟的上边大于水管的下边
-      strike = false;
+    let strike = true; // 假设撞上了
+    if(  bird.right < pipe.left  // 小鸟的右边小于水管的左边
+      || bird.bottom < pipe.top  // 小鸟的下边小于水管的上边
+      || bird.left > pipe.right  // 小鸟的左边大于水管的右边
+      || bird.top > pipe.bottom  // 小鸟的上边大于水管的下边
+      ){
+        // 没撞上
+        strike = false;
     }
     return strike;
   }
 
-  //判断小鸟的撞击事件(天、地、水管)
+  // 判断小鸟的撞击事件(天、地、水管)
   check(){
     const birds = this.dataStore.get('birds');
     const land = this.dataStore.get('land');
     const pipes = this.dataStore.get('pipes');
     const score = this.dataStore.get('score');
-    //判断与天地相撞
-    if(birds.birdsY[0]<0 || birds.birdsHeight[0]+birds.birdsY[0]>land.y){
-      //游戏结束
-      this.isGameOver = true;
-      return;
-    }
-    //判断与水管相撞
-    //构建小鸟的模型数据
-    const birdBorder = {
-      top:birds.birdsY[0],
-      right:birds.birdsX[0]+birds.birdsWidth[0],
-      bottom:birds.birdsY[0]+birds.birdsHeight[0],
-      left:birds.birdsX[0]
-    }
-    //循环水管遍历构建每一个水管的模型数据
-    for(let i=0;i<pipes.length;i++){
-      const pipe = pipes[i];
-      const pipeBorder={
-      top:pipe.y,
-      right:pipe.x + pipe.width,
-      bottom:pipe.y + pipe.height,
-      left:pipe.x
-      }
-      //判断小鸟与某个水管是否撞击
-      if(this.isStrike(birdBorder,pipeBorder)){
-        //相撞了
+
+    // 判断与天地相撞，没有无敌
+    if((birds.birdsY[0]<0 || birds.birdsHeight[0]+birds.birdsY[0]>land.y) && !birds.wudi ){
+      // 撞了，减命
+      birds.life--;
+      // 判断还有没有命
+      if(birds.life==0){
+        // 没，游戏结束
         this.isGameOver = true;
+        return ;
+      }else{
+        // 有，游戏不结束。改为无敌状态1秒
+        birds.wudi = true;
+        // 1秒后无敌消失
+        this.change();
         return ;
       }
     }
-    //小鸟越过水管,没有撞上，且当前状态允许加分，则加分
-    //越过的肯定是第一组水管，所以只需要判断小鸟与第一组水管的位置关系
+    // 判断与水管相撞
+    // 构建小鸟的模型数据
+    const birdBorder = {
+      top: birds.birdsY[0],
+      right: birds.birdsX[0] + birds.birdsWidth[0],
+      bottom: birds.birdsY[0] + birds.birdsHeight[0],
+      left: birds.birdsX[0]
+    }
+    // 循环水管，遍历构建每一个水管的模型数据
+    for(let i=0;i<pipes.length;i++){
+      const pipe = pipes[i];
+      const pipeBorder = {
+        top: pipe.y,
+        right: pipe.x + pipe.width,
+        bottom: pipe.y + pipe.height,
+        left: pipe.x
+      }
+      // 判断小鸟与某个水管是否撞击
+      if(this.isStrike(birdBorder,pipeBorder) && !birds.wudi){
+        // 撞了，减命
+        birds.life--;
+        // 判断还有没有命
+        if (birds.life == 0) {
+          // 没，游戏结束
+          this.isGameOver = true;
+          return;
+        } else {
+          // 有，游戏不结束。改为无敌状态1秒
+          birds.wudi = true;
+          // 1秒后无敌消失
+          this.change();
+          return;
+        }
+      }
+    }
+    // 小鸟越过水管，没有撞上，当前可以加分状态，加分
+    // 越过的肯定是第一组水管，所以只需要判断小鸟与第一组水管的位置关系
     if(birds.birdsX[0]>pipes[0].x+pipes[0].width && score.canAdd){
       score.score++;
-      //改变加分状态为不可加分
+      // 改变加分状态为不可加分
       score.canAdd = false;
     }
   }
 
-  //点击屏幕，小鸟向上飞一段距离
+
+  // 点击屏幕，小鸟向上飞一段距离
   birdsUp(){
-    const birds = this.dataStore.get('birds');//获取变量池中的小鸟对象
+    const birds = this.dataStore.get('birds'); // 获取变量池中的小鸟对象
     for(let i=0;i<3;i++){
       birds.y[i] = birds.birdsY[i];
     }
     birds.time = 0;
   }
 
-  //创建水管
+  // 创建水管
   createPipes(){
-    const minTop = this.dataStore.canvas.height/8; //最小值
-    const maxTop = this.dataStore.canvas.height/2; //最大值
-    const top = Math.random()*(maxTop-minTop)+minTop; //top值
-    this.dataStore.get("pipes").push(new UpPipe(top));
-    this.dataStore.get("pipes").push(new DownPipe(top));
+    const minTop = this.dataStore.canvas.height/8; // 最小值
+    const maxTop = this.dataStore.canvas.height/2; // 最大值
+    const top = Math.random()*(maxTop-minTop) + minTop; // top值
+    this.dataStore.get('pipes').push(new UpPipe(top));
+    this.dataStore.get('pipes').push(new DownPipe(top));
   }
 
-  //运行
+  // 运行
   run(){
-    //检查游戏是否结束
+    // 检查游戏是否结束
     this.check();
     if(!this.isGameOver){
-      //游戏未结束
-      //画背景图
-    this.dataStore.get("background").draw();
-   
-    //获取水管数组
-    const pipes = this.dataStore.get("pipes");
-    // this.createPipes(); //调用创建水管的方法
-    //创建水管之前先判断
-    //有没有出界,出界就将其从数组中删除
-    if(pipes[0].x<-pipes[0].width && pipes.length==4){
-      pipes.shift();
-      pipes.shift();
-      //修改加分状态为可以加分
-      this.dataStore.get('score').canAdd=true;
-    }
-    //创建水管的条件:前面一组水管有没有越过屏幕中央,如果越过,则开始创建下一组水管
-    const CanvasWidth = this.dataStore.canvas.width;
-    if(pipes[0].x<(CanvasWidth-pipes[0].width)/2 && pipes.length==2){
-      this.createPipes();
-    }
-    //遍历数组,画水管
-    pipes.forEach(pipe => {
-      pipe.draw();
-    }) 
-    //画地板图
-    this.dataStore.get("land").draw();
-    //画分数
-    this.dataStore.get('score').draw();
-    //获取小鸟并画出来
-    this.dataStore.get('birds').draw();
-    //循环运行
-    this.id = requestAnimationFrame(() => this.run());
-    
+      // 游戏未结束
+      // 画背景图
+      this.dataStore.get('background').draw();
+      // 获取水管数组
+      const pipes = this.dataStore.get('pipes');
+      // this.createPipes(); // 调用创建水管的方法
+      // 创建水管之前先判断
+      // 有没有出界的，出界就将其从数组中删除
+      if(pipes[0].x<-pipes[0].width && pipes.length==4){
+        pipes.shift();
+        pipes.shift();
+        // 修改加分状态为可以加分
+        this.dataStore.get('score').canAdd = true;
+      }
+      // 创建水管：前面一组水管有没有越过屏幕中央，如果越过，则开始创建下一组水管
+      const CanvasWidth = this.dataStore.canvas.width;
+      if(pipes[0].x<(CanvasWidth-pipes[0].width)/2 && pipes.length==2){
+        this.createPipes();
+      }
+      // 遍历数组，画水管
+      pipes.forEach(pipe=>{
+        pipe.draw();
+      });
+      // 画地板图
+      this.dataStore.get('land').draw();
+      // 画分数
+      this.dataStore.get('score').draw();
+      // 获取小鸟并画出来
+      this.dataStore.get('birds').draw();
+      if(this.dataStore.get('birds').wudi){
+        let ctx = this.dataStore.ctx;
+        ctx.fillStyle = 'rgba(150,100,50,0.7)';
+        let birds = this.dataStore.get('birds');
+        ctx.fillRect(birds.birdsX[0], birds.birdsY[0], birds.birdsWidth[0], birds.birdsHeight[0]);
+      }
+      // 循环运行
+      this.id = requestAnimationFrame(()=>this.run());
     }else{
       let t = new Tool();
       t.zhendong();
-      //游戏结束，停止环境渲染
+      t.voice('./audio/boom.mp3',false).play();
+      // 游戏结束，停止循环渲染
       cancelAnimationFrame(this.id);
-      //游戏结束，重新渲染一次(避免安卓贴图错乱)
+      // 游戏结束，重新渲染一次(避免安卓贴图错乱)
       this.dataStore.get('background').draw();
+      this.dataStore.get('land').draw();
       this.dataStore.get('pipes').forEach(p=>{
         p.draw();
       });
-      this.dataStore.get('land').draw();
       this.dataStore.get('birds').draw();
       this.dataStore.get('score').draw();
-      //画结束的按钮
+      // 画结束的按钮
       this.dataStore.get('startButton').draw();
-      //销毁数据
+      // 销毁数据
       this.dataStore.destroy();
     }
   }
-   
 }
+
+
+
+
+
+
--- js/player/Birds.js
+++ js/player/Birds.js
@@ -1,58 +1,62 @@
 import { Sprite } from "../base/Sprite.js";
 import { DataStore } from "../base/DataStore.js";
 
-//小鸟类
+// 小鸟类
 
 export class Birds extends Sprite{
-    constructor(){
-        const img = Sprite.getImage('birds');
-        super(img,0,0,img.width,img.height,0,0,img.width,img.height);
-        //设置三只小鸟的尺寸位置
-        //小鸟的宽是34，高24，上下边距10，左右边距9
-        this.clippingx = [9,9+34+18,9+34+18+34+18];//裁剪的x坐标
-        this.clippingY = [10,10,10];//裁剪的y坐标
-        this.clippingWidth = [34,34,34];//裁剪的宽度
-        this.clippingHeight = [24,24,24];//裁剪的高度
-        const canvas = DataStore.getInstance().canvas;
-        const birdX = canvas.width/4;//小鸟初始x坐标
-        this.birdsX = [birdX,birdX,birdX];
-        const land = Sprite.getImage('land');
-        const birdY = (canvas.height-land.height)/2;//小鸟初始y坐标
-        this.birdsY = [birdY,birdY,birdY];
-        this.birdsWidth = [34,34,34];//小鸟的宽度
-        this.birdsHeight = [24,24,24];//小鸟的高度
-        this.y = [birdY,birdY,birdY];//小鸟的实时y坐标
-        this.index = 0;//切换小鸟，实现动态效果
-        this.count = 0;//计数器
-        this.time = 0;//计时器，自由落体时间
-    }
-    draw(){
-        //小鸟的状态切换(翅膀方向的切换)
-        this.count += 0.2;
-        if(this.index>=2){
-            this.count=0;
-        }
-        this.index = Math.floor(this.count);
+  constructor(){
+    const img = Sprite.getImage('birds');
+    super(img,0,0,img.width,img.height,0,0,img.width,img.height);
+    // 设置三只小鸟的尺寸位置
+    // 小鸟的宽是34，高是24，上下边距是10，左右边距是9
+    this.clippingX = [9,9+34+18,9+34+18+34+18]; // 裁剪的x坐标
+    this.clippingY = [10,10,10]; // 裁剪的y坐标
+    this.clippingWidth = [34,34,34]; // 裁剪的宽度
+    this.clippingHeight = [24,24,24]; // 裁剪的高度
+    const canvas = DataStore.getInstance().canvas;
+    const birdX = canvas.width/4; // 小鸟初始x坐标
+    this.birdsX = [birdX,birdX,birdX];
+    const land = Sprite.getImage('land');
+    const birdY = (canvas.height-land.height)/2; // 小鸟初始y坐标
+    this.birdsY = [birdY,birdY,birdY];
+    this.birdsWidth = [34,34,34]; // 小鸟的宽度
+    this.birdsHeight = [24,24,24]; // 小鸟的高度
+    this.y = [birdY,birdY,birdY]; // 小鸟的实时y坐标
+    this.index = 0; // 切换小鸟，实现动态效果
+    this.count = 0; // 计数器
+    this.time = 0; // 计时器，自由落体时间
+    this.life = 3;
+    this.wudi = false;
+  }
 
-        //小鸟自由落体运行
-        const g = 0.6;//模拟重力加速度
-        //小鸟向上的位移量
-        const Y = 20;
-        //小鸟下落的距离    
-        const offsetY = (g*this.time*(this.time-Y))/2;
-        for(let i=0;i<3;i++){
-            this.birdsY[i] = this.y[i]+offsetY;
-        }
-        //时间变化
-        this.time++;
-
-        super.draw(this.img,this.clippingx[this.index]
-            ,this.clippingY[this.index],
-            this.clippingWidth[this.index],
-            this.clippingHeight[this.index],
-            this.birdsX[this.index],
-            this.birdsY[this.index],
-            this.birdsWidth[this.index],
-            this.birdsHeight[this.index]);
+  draw(){
+    // 小鸟的状态切换(翅膀方向的切换)
+    this.count += 0.2;
+    if(this.index>=2){
+      this.count = 0;
     }
+    this.index = Math.floor(this.count);
+
+    // 小鸟自由落体运动
+    const g = 0.98/2; // 模拟的重力加速度
+    // 小鸟向上的位移量
+    const Y = 30;
+    // 小鸟下落的距离
+    const offsetY = (g*this.time*(this.time-Y))/2;
+    for(let i=0;i<3;i++){
+      this.birdsY[i] = this.y[i] + offsetY;
+    }
+    this.time++;
+
+    super.draw(this.img, 
+              this.clippingX[this.index],
+              this.clippingY[this.index],
+              this.clippingWidth[this.index],
+              this.clippingHeight[this.index],
+              this.birdsX[this.index],
+              this.birdsY[this.index],
+              this.birdsWidth[this.index],
+              this.birdsHeight[this.index]
+      );
+  }
 }
